a951db9f43467265cdfccf63f1b19e854c3551e6
Diego Herrera <jdht1992@gmail.com>
2019-03-19 13:39:31 -0600

    adding method index

---
+++ string/string_method/index.py
@@ -0,0 +1,6 @@
+name = "Hello, my name is Juan Diego"
+#Find the first occurrence of the specified value.
+#returns -1 if the value is not found
+print(name.find("2", 0, 23))
+#the method generates an exception if the value is not found 
+print(name.index("o", 0, 23))
