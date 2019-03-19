4181b5ffb6dc3392d163ca1d530f318603d22b8c
Diego Herrera <jdht1992@gmail.com>
2019-03-19 13:26:35 -0600

    adding method split

--- string/string_method/replace.py
+++ string/string_method/replace.py
@@ -1,4 +1,3 @@
 # The replace() method replaces a string with another string:
 name = "Hello, my name is Juan Diego"
-
 print(name.replace("a", "e"))
---
+++ string/string_method/split.py
@@ -0,0 +1,3 @@
+# The split() method splits the string into substrings if it finds instances of the separator:
+name = "Hello, my name is Juan Diego"
+print(name.split(","))
