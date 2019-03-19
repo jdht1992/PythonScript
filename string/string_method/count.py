4b0c46ebdd65cc38a46f834e17b74a5b23c76a80
Diego Herrera <jdht1992@gmail.com>
2019-03-19 13:34:57 -0600

    adding method count

---
+++ string/string_method/count.py
@@ -0,0 +1,4 @@
+# The count () method returns the number of times a specified value appears in the string.
+name = "Hello, my name is Juan Diego"
+#string.count(value, start, end)
+print(name.count("e", 0, 12))
