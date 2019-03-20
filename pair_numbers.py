2705adb33d773b6e388950a54f4f582c6dd52733
Diego Herrera <jdht1992@gmail.com>
2019-03-20 16:21:03 -0600

    change example pair_number.py,adding the sum of all pair

--- pair_numbers.py
+++ pair_numbers.py
@@ -1,13 +1,16 @@
 # An even number is an integer that is divisible by two
 
-pair = []
-no_pair = []
-for num in range(1, 10):
-    if num % 2 == 0:
-        pair.append(num)
-    else: #elif num % 2 != 0:
-        no_pair.append(num)
+# pair = []
+# no_pair = []
+# sum_pair = 0
+# for num in range(1, 10):
+#     if num % 2 == 0:
+#         pair.append(num)
+#         sum_pair += num
+#     else: #elif num % 2 != 0:
+#         no_pair.append(num)
 
-print(f'Numbers pair {pair}')
-print(f'No Numbers pair {no_pair}')
+# print(f'Sum pair: {sum_pair}')
+# print(f'Numbers pair {pair}')
+# print(f'No Numbers pair {no_pair}')
 
